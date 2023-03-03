9f8f27a831b854878fdbd19bc11e50bd80d4ed6a
Step1017 <s.aveta@yahoo.com>
2023-03-03 16:24:33 +0100

    Costruttore della classe

--- index.php
+++ index.php
@@ -5,5 +5,12 @@ class Movie {
     public $title;
     public $director;
     public $year; 
+
+    // Costruttore della classe
+    public function __construct($title, $director, $year) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+    }
 }
 ?>
