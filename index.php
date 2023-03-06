f2091e161e2aef466ab13d24fd8d7a94431f35ac
Step1017 <s.aveta@yahoo.com>
2023-03-06 17:06:29 +0100

    Stampa in pagina con foreach

--- index.php
+++ index.php
@@ -70,32 +70,90 @@ class Movie {
 }
 
 // Istanziazione degli oggetti Movie
-$movie1 = new Movie("Iron Man", "Jon Favreau", "2008",["action","fantasy","adventure"]);
-$movie2 = new Movie("Thor", "Kenneth Branagh", "2011",["action","fantasy","adventure"]);
-$movie3 = new Movie("Captain America: The First Avenger", "Joe Johnston", "2011",["action","fantasy","adventure"]);
-$movie4 = new Movie("Marvel's The Avengers", "Joss Whedon", "2012",["action","fantasy","adventure"]);
-$movie5 = new Movie("Guardiani della Galassia", "James Gunn", "2014",["action","fantasy","adventure","comedy"]);
-$movie6 = new Movie("Avengers: Age of Ultron", "Joss Whedon", "2015",["action","drama"]);
-$movie7 = new Movie("Ant-Man", "Peyton Reed", "2015",["action","fantasy","comedy"]);
-$movie8 = new Movie("Doctor Strange", "Scott Derrickson", "2016",["action","fantasy","drama"]);
-$movie9 = new Movie("Spider-Man: Homecoming", "Jon Watts", "2017",["action"]);
-$movie10 = new Movie("Black Panther", "Ryan Coogler", "2018",["action"]);
-$movie11 = new Movie("Avengers: Infinity War", "Anthony Russo & Joe Russo", "2018",["action","drama"]);
-$movie12 = new Movie("Captain Marvel", "Anna Boden; Ryan Fleck", "2019",["action"]);
-$movie13 = new Movie("Avengers: Endgame", "Anthony Russo & Joe Russo", "2019",["action","drama","fantasy"]);
-$movie14 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
-$movie15 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
-$movie16 = new Movie("Shang-Chi e la Leggenda dei Dieci Anelli", "Destin Daniel Cretton", "2021",["action"]);
-$movie17 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
-$movie18 = new Movie("Eternals", "Chloé Zhao", "2021",["action"]);
-$movie19 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
+// $movie1 = new Movie("Iron Man", "Jon Favreau", "2008",["action","fantasy","adventure"]);
+// $movie2 = new Movie("Thor", "Kenneth Branagh", "2011",["action","fantasy","adventure"]);
+// $movie3 = new Movie("Captain America: The First Avenger", "Joe Johnston", "2011",["action","fantasy","adventure"]);
+// $movie4 = new Movie("Marvel's The Avengers", "Joss Whedon", "2012",["action","fantasy","adventure"]);
+// $movie5 = new Movie("Guardiani della Galassia", "James Gunn", "2014",["action","fantasy","adventure","comedy"]);
+// $movie6 = new Movie("Avengers: Age of Ultron", "Joss Whedon", "2015",["action","drama"]);
+// $movie7 = new Movie("Ant-Man", "Peyton Reed", "2015",["action","fantasy","comedy"]);
+// $movie8 = new Movie("Doctor Strange", "Scott Derrickson", "2016",["action","fantasy","drama"]);
+// $movie9 = new Movie("Spider-Man: Homecoming", "Jon Watts", "2017",["action"]);
+// $movie10 = new Movie("Black Panther", "Ryan Coogler", "2018",["action"]);
+// $movie11 = new Movie("Avengers: Infinity War", "Anthony Russo & Joe Russo", "2018",["action","drama"]);
+// $movie12 = new Movie("Captain Marvel", "Anna Boden; Ryan Fleck", "2019",["action"]);
+// $movie13 = new Movie("Avengers: Endgame", "Anthony Russo & Joe Russo", "2019",["action","drama","fantasy"]);
+// $movie14 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
+// $movie15 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
+// $movie16 = new Movie("Shang-Chi e la Leggenda dei Dieci Anelli", "Destin Daniel Cretton", "2021",["action"]);
+// $movie17 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
+// $movie18 = new Movie("Eternals", "Chloé Zhao", "2021",["action"]);
+// $movie19 = new Movie("Black Widow", "Cate Shortland", "2021",["action"]);
+
+//trasformo oggetti Movie in array
+$movies = array (
+new Movie("Iron Man", "Jon Favreau", "2008",["action","fantasy","adventure"]),
+new Movie("Thor", "Kenneth Branagh", "2011",["action","fantasy","adventure"]),
+new Movie("Captain America: The First Avenger", "Joe Johnston", "2011",["action","fantasy","adventure"]),
+new Movie("Marvel's The Avengers", "Joss Whedon", "2012",["action","fantasy","adventure"]),
+new Movie("Guardiani della Galassia", "James Gunn", "2014",["action","fantasy","adventure","comedy"]),
+new Movie("Avengers: Age of Ultron", "Joss Whedon", "2015",["action","drama"]),
+new Movie("Ant-Man", "Peyton Reed", "2015",["action","fantasy","comedy"]),
+new Movie("Doctor Strange", "Scott Derrickson", "2016",["action","fantasy","drama"]),
+new Movie("Spider-Man: Homecoming", "Jon Watts", "2017",["action"]),
+new Movie("Black Panther", "Ryan Coogler", "2018",["action"]),
+new Movie("Avengers: Infinity War", "Anthony Russo & Joe Russo", "2018",["action","drama"]),
+new Movie("Captain Marvel", "Anna Boden; Ryan Fleck", "2019",["action"]),
+new Movie("Avengers: Endgame", "Anthony Russo & Joe Russo", "2019",["action","drama","fantasy"]),
+new Movie("Black Widow", "Cate Shortland", "2021",["action"]),
+new Movie("Black Widow", "Cate Shortland", "2021",["action"]),
+new Movie("Shang-Chi e la Leggenda dei Dieci Anelli", "Destin Daniel Cretton", "2021",["action"]),
+new Movie("Black Widow", "Cate Shortland", "2021",["action"]),
+new Movie("Eternals", "Chloé Zhao", "2021",["action"]),
+new Movie("Black Widow", "Cate Shortland", "2021",["action"])
+);
 ?>
 
-<?php 
-echo '<h2>Movie 1:</h2>';
-var_dump($movie1);
+<!-- <?php 
+// echo '<h2>Movie 1:</h2>';
+// var_dump($movie1);
 
-echo '<h2>Movie 2:</h2>';
-var_dump($movie2);
+// echo '<h2>Movie 2:</h2>';
+// var_dump($movie2);
 
-?>
+// ?> -->
+
+
+
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+        <title>PHP Movie</title>
+    </head>
+    <body>
+        <div class="container mt-5">
+            <table class="table table-warning">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Director</th>
+                        <th scope="col">Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($movies as $movie) : ?>
+                        <tr>
+                            <td><?php echo $movie->getMovieTitle() ?></td>
+                            <td><?php echo $movie->getMovieDirector() ?></td>
+                            <td><?php echo $movie->getMovieYear() ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
